17ba0a140afeb67fda453e8723b9e270b2ca33b0
jdu211171 <211171m@jdu.uz>
2024-10-18 21:24:24 +0500

    double expo-build-service sub folder fixed

--- install_remote_server.sh
+++ install_remote_server.sh
@@ -8,17 +8,14 @@ REMOTE_HOST="$SERVER_IP"
 REMOTE_DIR="/home/$REMOTE_USER/expo-build-service"
 REMOTE_PASSWORD="$REMOTE_PASSWORD"
 
-# Create a tarball of the current directory in a temporary directory
+# Create a tarball of the contents of the current directory in a temporary directory
 TEMP_DIR=$(mktemp -d)
 TARBALL="$TEMP_DIR/expo-build-service.tar.gz"
 
 CURRENT_DIR="$(pwd)"
-CURRENT_DIR_NAME="$(basename "$CURRENT_DIR")"  # Should be expo-build-service
-PARENT_DIR="$(dirname "$CURRENT_DIR")"
 
-cd "$PARENT_DIR"
-tar --exclude="$TARBALL" -czf "$TARBALL" "$CURRENT_DIR_NAME"
-cd "$CURRENT_DIR"  # Go back to the original directory
+# Tar the contents of the current directory without including the directory itself
+tar --exclude="$TARBALL" -czf "$TARBALL" -C "$CURRENT_DIR" .
 
 # Ensure the remote directory exists
 sshpass -p "$REMOTE_PASSWORD" ssh "$REMOTE_USER@$REMOTE_HOST" "mkdir -p $REMOTE_DIR"
@@ -32,11 +29,11 @@ sshpass -p "$REMOTE_PASSWORD" ssh "$REMOTE_USER@$REMOTE_HOST" << EOF
   cd $REMOTE_DIR
   tar -xzf $(basename $TARBALL)
   rm $(basename $TARBALL)
-  if [ ! -d "expo-build-service" ]; then
-    echo "Error: Directory expo-build-service does not exist after extraction"
+  # Check for a key file to ensure extraction was successful
+  if [ ! -f "install_server.sh" ]; then
+    echo "Error: install_server.sh does not exist after extraction"
     exit 1
   fi
-  cd expo-build-service
   chmod +x install_server.sh
   echo "$REMOTE_PASSWORD" | sudo -S ./install_server.sh
 EOF
