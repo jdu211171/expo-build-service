036093d7e64586e75be49541bf53660d8b804b48
jdu211171 <123369867+jdu211171@users.noreply.github.com>
2024-12-06 21:05:29 +0500

    Update install_corretto.sh

--- install_corretto.sh
+++ install_corretto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-JDK_URL="https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.tar.gz"
+JDK_URL="https://corretto.aws/downloads/resources/17.0.13.11.1/amazon-corretto-17.0.13.11.1-linux-x64.tar.gz"
 JDK_DIR="$HOME/.jdks"
-JDK_TAR="amazon-corretto-17-x64-linux-jdk.tar.gz"
+JDK_TAR="amazon-corretto-17.0.13.11.1-linux-x64.tar.gz"
+DESIRED_DIR_NAME="amazon-corretto"
 
 mkdir -p $JDK_DIR
 if [ $? -ne 0 ]; then
@@ -30,22 +30,28 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-rm $JDK_TAR
-if [ $? -ne 0 ]; then
-    echo "Failed to remove $JDK_TAR"
-    exit 1
-fi
-
 EXTRACTED_DIR=$(tar -tzf $JDK_TAR | head -1 | cut -f1 -d"/")
 if [ -z "$EXTRACTED_DIR" ]; then
     echo "Failed to determine the extracted directory"
     exit 1
 fi
 
-echo "Setting JAVA_HOME and updating PATH..."
-export JAVA_HOME="$JDK_DIR/$EXTRACTED_DIR"
-export PATH="$PATH:$JAVA_HOME/bin"
+echo "Renaming extracted directory to $DESIRED_DIR_NAME..."
+mv "$JDK_DIR/$EXTRACTED_DIR" "$JDK_DIR/$DESIRED_DIR_NAME"
+if [ $? -ne 0 ]; then
+    echo "Failed to rename directory from $EXTRACTED_DIR to $DESIRED_DIR_NAME"
+    exit 1
+fi
 
+rm $JDK_TAR
+if [ $? -ne 0 ]; then
+    echo "Failed to remove $JDK_TAR"
+    exit 1
+fi
+
+echo "Setting JAVA_HOME and updating PATH..."
+export JAVA_HOME="$JDK_DIR/$DESIRED_DIR_NAME"
+export PATH="$PATH:$JAVA_HOME/bin"
 if [ ! -d "$JAVA_HOME" ]; then
     echo "JAVA_HOME is set to an invalid directory: $JAVA_HOME"
     exit 1
